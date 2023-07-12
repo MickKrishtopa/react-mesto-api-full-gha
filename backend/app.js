578af51e90b84caa030af05b1c75da57f2d1eac1
Mikhail Krishtopa <m.a.krishtopa@yandex.ru>
2023-07-12 13:50:26 +0400

    fix: make back PORT = 3000

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const cookieParser = require('cookie-parser'); // check
 const routes = require('./routes/routes');
 const { errorHandler } = require('./middlewares/errorHandler');
 
-const { PORT = 4000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb', CORS_URL = 'http://localhost:3000' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb', CORS_URL = 'http://localhost:3000' } = process.env;
 
 mongoose
   .connect(DB_URL, {
